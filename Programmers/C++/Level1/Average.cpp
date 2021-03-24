51787f5d770d418b5822d21a0a5034344d6ac67f
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2021-03-24 09:46:25 +0900

    [Programmers] 평균 구하기
    프로그래머스 코딩테스트 연습문제
    "평균 구하기" C++ 코드입니다.

---
+++ Programmers/C++/Level1/Average.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    int len = arr.size();
+    for(int i = 0; i < len; i++)
+    {
+        answer += arr[i];
+    }
+    answer /= len;
+    
+    return answer;
+}
