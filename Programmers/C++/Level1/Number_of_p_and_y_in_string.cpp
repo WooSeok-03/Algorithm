48142028266ab3492d4dbf42d10d75147e40640a
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2021-04-14 11:18:10 +0900

    [Programmers] 문자열 내 p와 y의 개수
    프로그래머스 코딩테스트 연습문제
    "문자열 내 p와 y의 개수" C++ 코드입니다.

---
+++ Programmers/C++/Level1/Number_of_p_and_y_in_string.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+bool solution(string s)
+{
+    int len = s.length();
+    int p = 0;
+    int y = 0;
+    
+    for(int i = 0; i < len; i++)
+    {
+        if(s[i] == 'p' || s[i] == 'P') p++;
+        if(s[i] == 'y' || s[i] == 'Y') y++;
+    }
+
+    return p == y;
+}
