be8fccce043331e67f55072b9a95ae516c9c8785
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2021-04-14 10:44:25 +0900

    [Programmers] 문자열 다루기 기본
    프로그래머스 코딩테스트 연습문제
    "문자열 다루기 기본" C++ 코드입니다.

---
+++ Programmers/C++/Level1/String_handling_basics.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 내가 코딩한 코드
+bool solution(string s) {
+    bool answer = false;
+    int len = s.length();
+    
+    if(len == 4 || len == 6)
+    {
+        for(int i = 0; i < len; i++)
+        {
+            if(s[i] >= '0' && s[i] <= '9')
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+                break;
+            }
+        }
+    }
+    
+    return answer;
+}
+
+// isdigit함수 사용한 코드
+bool solution(string s) {
+    bool answer = true;
+	int len = s.length();
+
+    for (int i = 0; i < len; i++)
+    {
+        if (!isdigit(s[i])) answer = false;
+		if (!(len == 4 || len == 6)) answer = false;
+    }
+
+    return answer;
+}
+
+// isdigit함수는 숫자를 판단하는 함수이다.
+// char 타입이 10진수 숫자로 변경이 가능하면 숫자를 return / 아니면 0(false)를 반환하는 함수이다.
