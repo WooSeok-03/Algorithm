3d1a6aec4cbf47273abf67ed93c61824d65de950
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2021-04-16 09:59:30 +0900

    [Programmers] 콜라츠 추측
    프로그래머스 코딩테스트 연습문제
    "콜라츠 추측" C 코드입니다.

---
+++ Programmers/C/Level1/Collatz_conjecture.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num) {
+    int answer = 0;
+    
+    while(num != 1)
+    {   
+        if(answer >= 500 || num < 1)
+        {
+            answer = -1;
+            break;
+        }
+        
+        if(num % 2 == 0)
+        {
+            num /= 2;
+        }
+        else
+        {
+            num *= 3;
+            num += 1;
+        }
+        answer++;
+        
+    }
+    return answer;
+}
