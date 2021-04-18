65d400b7d3fa68b1080fcf450804f58e96ba6bb7
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2021-04-18 16:13:08 +0900

    [Programmers] 내적
    프로그래머스 코딩테스트 연습문제
    "내적" Python 코드입니다.
    ( +주석 추가 )

--- Programmers/Python/Level1/Dot_product.py
+++ Programmers/Python/Level1/Dot_product.py
@@ -5,3 +5,14 @@ def solution(a, b):
         answer += a[i] * b[i]
     
     return answer
+    
+
+#def solution(a, b):
+#    answer = 0
+#    for x, y in zip(a,b):  # zip() : 동일한 개수로 이루어진 자료형을 묶어 주는 역할을 하는 함수
+#        answer += x*y
+#    return answer
+
+# zip() 예시
+# >>> list(zip([1, 2, 3], [4, 5, 6]))
+# [(1, 4), (2, 5), (3, 6)]
