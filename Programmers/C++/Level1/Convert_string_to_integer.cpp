59a6f0801f42c86e6f6641551aed658aac018cc2
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2021-04-18 16:36:38 +0900

    [Programmers] 문자열을 정수로 바꾸기
    프로그래머스 코딩테스트 연습문제
    "문자열을 정수로 바꾸기" C++ 코드입니다.

---
+++ Programmers/C++/Level1/Convert_string_to_integer.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    return stoi(s); // stoi함수는 string을 int로 변환해주는 함수이다.
+}
+
+// C언어에서의 atoi함수와 같은 기능이다.
+// ( C언어 : atoi / C++ : stoi ) 
