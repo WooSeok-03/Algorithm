5d2ba68999593a994bcd8a7be8f98cdb96df4cef
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2021-04-18 14:17:58 +0900

    [Programmers] 정수 제곱근 판별
    프로그래머스 코딩테스트 연습문제
    "정수 제곱근 판별" Python 코드입니다.
    ( +주석 추가 )

--- Programmers/Python/Level1/Discriminate_Integer_square_root.py
+++ Programmers/Python/Level1/Discriminate_Integer_square_root.py
@@ -3,8 +3,8 @@ import math
 def solution(n):
     answer = 0
     
-    n **= (1/2)
-    a = math.trunc(n)
+    n **= (1/2)         # **는 제곱하는 연산이다. ( n ** (1/2)를 하면 제곱근을 구할 수 있다.
+    a = math.trunc(n)   # math.trunc()함수는 소수점을 버리는 함수이다.
     
     if n - a > 0: return -1
     n += 1
